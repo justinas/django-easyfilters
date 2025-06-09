ccf4273c3e61ca247b191ec451090637caa040c5
Justinas Stankevicius <justinas@justinas.org>
2025-06-09 16:48:22 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(*names, **kwargs):
 
 setup(
     name="django-easyfilters-ex",
-    version='0.7.0',
+    version='0.7.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
